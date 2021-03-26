b9e8cee5d5dffd4541826482c3d3d2b44c74f2b2
Shibumishra <sibumishra487@gmail.com>
2021-03-26 23:17:19 +0530

    practice

---
+++ practice_If_Else.js
@@ -0,0 +1,25 @@
+console.log("1 START =====================");
+var year = 2020;
+
+if(year % 4 === 0){
+  if(year % 100 === 0){
+    if(year % 400 === 0){
+       console.log("this year " + year + " is a leap year"); 
+    }else{
+      console.log("this year " + year + " not leap year");
+    }
+    
+  }else{
+  console.log("this year " + year + " is a leap year");  
+  }
+  
+}else{
+  console.log("this year " + year + " not leap year");
+}
+
+
+
+console.log("2 START =====================");
+var age = 19;
+ console.log((age >= 18) ? "please voteing" : "please not voteing");
+
